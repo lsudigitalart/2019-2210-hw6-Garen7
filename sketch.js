e8613dda180dd1c2b32d352887c8da246ee9f11e
Garen Crownguard <22408776+Garen7@users.noreply.github.com>
2019-10-08 14:20:25 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,29 @@
-function crop(image, rMin = 0, rMax = 256, gMin = 0, gMax = 256, bMin = 0, bMax = 256, alpha = 0){
+function crop(image, limits){
+  if(!limits) limits = {}
+  if(!limits.rMin) limits.rMin = 0
+  if(!limits.rMax) limits.rMax = 256
+  if(!limits.gMin) limits.gMin = 0
+  if(!limits.gMax) limits.gMax = 256
+  if(!limits.bMin) limits.bMin = 0
+  if(!limits.bMax) limits.bMax = 256
+  if(!limits.alpha) limits.alpha = 0
+  
   image.loadPixels()
 
   for(var index = 0; index < image.pixels.length; index += 4){
     //red
-    if(image.pixels[index] < rMin || image.pixels[index] > rMax){
-      image.pixels[index+3] = alpha
+    if(image.pixels[index] < limits.rMin || image.pixels[index] > limits.rMax){
+      image.pixels[index+3] = limits.alpha
       continue
     }
     //green
-    else if(image.pixels[index+1] < gMin || image.pixels[index+1] > gMax){
-      image.pixels[index+3] = alpha
+    else if(image.pixels[index+1] < limits.gMin || image.pixels[index+1] > limits.gMax){
+      image.pixels[index+3] = limits.alpha
       continue
     }
     //blue
-    else if(image.pixels[index+2] < bMin || image.pixels[index+2] > bMax){
-      image.pixels[index+3] = alpha
+    else if(image.pixels[index+2] < limits.bMin || image.pixels[index+2] > limits.bMax){
+      image.pixels[index+3] = limits.alpha
       continue
     }
   }
